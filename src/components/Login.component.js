59ba42a380a34f01c2030af61e197c7c9f997650
mae-yosores2k19 <jessamaehortadoyosores@gmail.com>
2019-11-25 11:11:36 +0800

    mrclay update login card 11/25/19--11:11 am

--- src/components/Login.component.js
+++ src/components/Login.component.js
@@ -3,7 +3,7 @@ import { Button } from "react-bootstrap";
 import "./Login.css";
 import { Link } from "react-router-dom";
 import { Card } from "primereact/card";
-import { Input } from "semantic-ui-react";
+import { Input, Icon, type } from "semantic-ui-react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 //import Map from "./Map";
 import Menu from "./Menu";
@@ -11,6 +11,9 @@ import Menu from "./Menu";
 export default function Login(props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  var [inpType, setInputType] = useState("password");
+  var [lastType, setLastType] = useState(true);
+  var [eyeIcon, setEyeIcon] = useState("eye slash");
 
   function validateForm() {
     return email.length > 0 && password.length > 0;
@@ -18,6 +21,19 @@ export default function Login(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
+  }
+  function handleEyeClick(event){
+    event.preventDefault();
+    if(lastType){
+      setInputType("text");
+      setLastType(false);
+      setEyeIcon("eye");
+    }else{
+      setInputType("password");
+      setLastType(true);
+      setEyeIcon("eye slash");
+    }
+    
   }
 
   return (
@@ -38,7 +54,9 @@ export default function Login(props) {
             <br />
             <Input
               className="inpLogin"
-              icon="eye"
+              // icon="eye"
+              type={inpType}
+              icon={<Icon name={eyeIcon} link onClick={handleEyeClick}/>}
               size="large"
               placeholder="Password"
             />
